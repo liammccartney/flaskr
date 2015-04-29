9730c4b6440d4af63d4552301e7e541a326df3c6
Liam McCartney <liam.mccartney@gmail.com>
2015-04-29 16:22:04 -0400

    runs init.db, fixes problems

--- flaskr.py
+++ flaskr.py
@@ -6,13 +6,12 @@ from contextlib import closing
 # create our little application
 app = Flask(__name__)
 
-app.config.update(dict(
-    DATABASE = '/tmp/flaskr.db',
-    DEBUG = True,
-    SECRET_KEY = 'development key',
-    USERNAME = 'admin',
-    PASSWORD = 'default'
-))
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+app.config.from_object(__name__)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
@@ -51,14 +50,15 @@ def add_entry():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-    if request.form['username'] != app.config['USERNAME']:
-        error = 'Invalid username'
-    elif request.form['password'] != app.config['PASSWORD']:
-        error = 'Invalid password'
-    else:
-        session['logged_in'] = True
-        flash('You were logged in')
-        return redirect(url_for('show_entries'))
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
 
 @app.route('/logout')
