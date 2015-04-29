18e73eaf4b17091fde38b16dd330904822f5e209
Liam McCartney <liam.mccartney@gmail.com>
2015-04-29 15:05:57 -0400

    moves app init up

--- flaskr.py
+++ flaskr.py
@@ -8,6 +8,10 @@ def init_db():
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
+# create our little application
+app = Flask(__name__)
+app.config.from_object(__name__)
+
 
 
 # configuration
