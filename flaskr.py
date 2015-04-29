e480f6427dfa8889d37ed7601a2b5ab59aedf989
Liam McCartney <liam.mccartney@gmail.com>
2015-04-29 15:40:26 -0400

    moves some things around

--- flaskr.py
+++ flaskr.py
@@ -3,15 +3,25 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from contextlib import closing
 
+# create our little application
+app = Flask(__name__)
+
+app.config.update(dict(
+    DATABASE = '/tmp/flaskr.db',
+    DEBUG = True,
+    SECRET_KEY = 'development key',
+    USERNAME = 'admin',
+    PASSWORD = 'default'
+))
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
-# create our little application
-app = Flask(__name__)
-app.config.from_object(__name__)
-
 
 @app.before_request
 def before_request():
@@ -22,16 +32,6 @@ def teardown_request(exception):
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
-
-# configuration
-DATABASE = '/tmp/flaskr.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
 
 @app.route('/')
 def show_entries():
