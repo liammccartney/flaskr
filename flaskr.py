922cd07b8d49aa612dddc16aaba28987a5c3208c
Liam McCartney <liam.mccartney@gmail.com>
2015-04-29 15:08:18 -0400

    moves app init up

--- flaskr.py
+++ flaskr.py
@@ -13,6 +13,15 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
 
 # configuration
 DATABASE = '/tmp/flaskr.db'
@@ -20,10 +29,6 @@ DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
-
-# create our little application
-app = Flask(__name__)
-app.config.from_object(__name__)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
