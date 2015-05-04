10a072e96bc37d16cf1e3597f02ec2a30e0a1a10
Liam McCartney <liam.mccartney@gmail.com>
2015-05-04 12:50:44 -0400

    adds missing stuff that I lost in merge conflicts

--- flaskr.py
+++ flaskr.py
@@ -23,6 +23,61 @@ app.config['LDAP_USER_SEARCH_SCOPE'] = os.environ.get('LDAP_USER_SEARCH_SCOPE')
 login_manager = LoginManager(app)
 ldap_manager = LDAP3LoginManager(app)
 
+users = {}
+
+# Declare an Object Model for the user, and make it comply with the
+# flask-login UserMixin mixin.
+class User(UserMixin):
+    def __init__(self, dn, username, data):
+        self.dn = dn
+        self.username = username
+        self.data = data
+
+    def __repr__(self):
+        return self.dn
+
+    def get_id(self):
+        return self.dn
+
+    def is_anonymous(self):
+        return False
+
+
+# Declare a User Loader for Flask-Login.
+# Simply returns the User if it exists in our 'database', otherwise
+# returns None.
+@login_manager.user_loader
+def load_user(id):
+    if id in users:
+        return users[id]
+    return None
+
+
+# Declare The User Saver for Flask-Ldap3-Login
+# This method is called whenever a LDAPLoginForm() successfully validates.
+# Here you have to save the user, and return it so it can be used in the
+# login controller.
+@ldap_manager.save_user
+def save_user(dn, username, data, memberships):
+    user = User(dn, username, data)
+    users[dn] = user
+    return user
+
+@app.route('/')
+def home():
+    # Redirect users who are not logged in.
+    if not current_user or current_user.is_anonymous():
+        return redirect(url_for('login'))
+
+    # User is logged in, so show them a page with their cn and dn.
+    template = """
+    <h1>Welcome: {{ current_user.data.cn }}</h1>
+    <h2>{{ current_user.dn }}</h2>
+    """
+
+    return render_template_string(template)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     template = """
