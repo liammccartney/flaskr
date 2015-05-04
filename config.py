a9411c068da3611f2bcb9e780e290f4b2e7f6a60
Liam McCartney <liam.mccartney@gmail.com>
2015-05-04 10:00:03 -0400

    loads env, sets db url

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
+import os
+import dotenv
+
+dotenv.load_dotenv(".env")
+
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENDABLED = True
     SECRET_KEY = 'this-oughta-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 class ProductionConfig(Config):
     DEBUG = False
