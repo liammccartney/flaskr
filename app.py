7cfa51dc1a29580267ab2ff52c8535e906b65970
Liam McCartney <liam.mccartney@gmail.com>
2015-05-04 15:49:06 -0400

    fixes stuff I did wrong in vim :/

--- app.py
+++ app.py
@@ -85,44 +85,50 @@ def home():
     # User is logged in, so show them a page with their cn and dn.
     errors = []
     results = {}
-    if request.method == 'POST':
+    if request.method == "POST":
+        # get url that the person has entered
         try:
             url = request.form['url']
             r = requests.get(url)
         except:
             errors.append(
-                    "can't do it, bro"
+                "Unable to get URL. Please make sure it's valid and try again."
             )
-            return render_template('index.html', errors=errors, results=results)
+            return render_template('index.html', errors=errors)
         if r:
+
+            # text processing
             raw = BeautifulSoup(r.text).get_text()
-            nltk.data.path.append('./nltk_data/')
+            nltk.data.path.append('./nltk_data/')  # set the path
             tokens = nltk.word_tokenize(raw)
             text = nltk.Text(tokens)
 
+            # remove punctuation, count raw words
             nonPunct = re.compile('.*[A-Za-z].*')
             raw_words = [w for w in text if nonPunct.match(w)]
-            raw_words_count = Counter(raw_words)
+            raw_word_count = Counter(raw_words)
 
+            # stop words
             no_stop_words = [w for w in raw_words if w.lower() not in stops]
             no_stop_words_count = Counter(no_stop_words)
 
+            # save the results
             results = sorted(
-                    no_stop_words_count.items(),
-                    key = operator.itemgetter(1),
-                    reverse=True
-             )
+                no_stop_words_count.items(),
+                key=operator.itemgetter(1),
+                reverse=True
+            )[:10]
             try:
                 result = Result(
-                        url=url,
-                        result_all=raw_words_count,
-                        result_no_stop_words=no_stop_words_count
+                    url=url,
+                    result_all=raw_word_count,
+                    result_no_stop_words=no_stop_words_count
                 )
                 db.session.add(result)
                 db.session.commit()
             except:
-                errors.append('Unable to add item to db')
-        return render_template('index.html', errors=errors, results=results)
+                errors.append("Unable to add item to database.")
+    return render_template('index.html', errors=errors, results=results)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
