ebdace80aeb99b34719e1c308f41328ecbd48134
Liam McCartney <liam.mccartney@gmail.com>
2015-05-05 09:52:46 -0400

    adds angular stuffs

---
+++ static/main.js
@@ -0,0 +1,24 @@
+  'use strict';
+
+  angular.module('WordcountApp', [])
+
+  .controller('WordcountController', ['$scope', '$log', '$http', function($scope, $log, $http) {
+  $scope.getResults = function() {
+
+    $log.log("test");
+
+    // get the URL from the input
+    var userInput = $scope.input_url;
+    // fire the API request
+    $http.post('/start', {"url": userInput}).
+      success(function(results) {
+        $log.log(results);
+      }).
+      error(function(error) {
+        $log.log(error);
+      });
+
+  };
+  }
+
+  ]);
